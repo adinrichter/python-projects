ff37a37563b8669b301538df8c518083ff82fb2b
adinrichter <adinrichter@gmail.com>
2022-08-10 14:02:03 -0700

    Updated assignment to match criteria more accurately

--- classes.py
+++ classes.py
@@ -14,53 +14,19 @@ class Organism:
 
 # child class instance
 class Human(Organism):
-    name = 'MacGuyver'
-    species = 'Homosapien'
+    name = 'Steve Jobs'
+    species = 'Human'
     legs = 2
     arms = 2
     origin = 'Earth'
 
-    def ingenuity(self):
-            return '\nCreates a deadly weapon using only a paper clip, chewing gum, and a roll of duct tape!'
+    def information(self):
+            return '\nCreated the company computer company Apple.'
 
-
-# another child class instance
-class Dog(Organism):
-    name = 'Spot'
-    species = 'Canine'
-    legs = 4
-    arms = None
-    dna = 'Sequence B'
-    origin = 'Earth'
-
-    def bite(self):
-        return '\nEmits a loud, menacing growl and bites down ferociously on its target!'
-
-
-# another child class instance
-class Bacterium(Organism):
-    name = 'X'
-    species = 'Bacteria'
-    legs = None
-    arms = None
-    dna = 'Sequence C'
-    origin = 'Mars'
-
-    def replication(self):
-        return '\nThe cells begin to devide and multiple into two seperate organisms!'
         
 
 if __name__ == '__main__':
     human = Human()
     print(human.information())
-    print(human.ingenuity())
-
-    dog = Dog()
-    print(dog.information())
-    print(dog.bite())
-
-    bacteria = Bacterium()
-    print(bacteria.information())
-    print(bacteria.replication())
 
 
