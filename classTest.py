ba0e0d6450d0038225964d7fe56a373c2fcb98e4
adinrichter <adinrichter@gmail.com>
2022-08-11 15:18:41 -0700

    Update classTest.py

--- classTest.py
+++ classTest.py
@@ -1,5 +1,7 @@
+# creates a class to store private information
 class Protected:
     def __init__(self):
+        # creates protected and private variables
         self._protectedInfo = 'secret information'
         self.__privateInfo = 'even more secret information'
         
