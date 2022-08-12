675e405d79f8ecb1304deb7a25a06318de4de6db
adinrichter <adinrichter@gmail.com>
2022-08-12 10:34:27 -0700

    Update abstraction_test.py

--- abstraction_test.py
+++ abstraction_test.py
@@ -1,1 +1,19 @@
 
+from abc import ABC, abstractmethod
+
+class website(ABC):
+    def buyAccess(self, time):
+        print('Thank you, you now have access. Your subscription will end in {} days'.format(time))
+    @abstractmethod
+    def useAccess(self, time):
+        pass
+
+class videos(website):
+    def useAccess(self, time):
+        print('You now have access for {} days.'.format(time))
+
+
+user = videos()
+
+user.buyAccess('30')
+user.useAccess('30')
