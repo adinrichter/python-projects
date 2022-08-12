ca54fa8d7cbc228443a4eab0d4a52a3039b6b86f
adinrichter <adinrichter@gmail.com>
2022-08-12 10:05:38 -0700

    Updated classTest to fit criteria

--- classTest.py
+++ classTest.py
@@ -4,4 +4,22 @@ class Protected:
         # creates protected and private variables
         self._protectedInfo = 'secret information'
         self.__privateInfo = 'even more secret information'
+
+    def getPrivate(self):
+        return self.__privateInfo
+
+    def setPrivate(self, input):
+        self.__privateInfo = input
         
+
+
+usingSecret = Protected()
+
+print(usingSecret._protectedInfo)
+usingSecret._protectedInfo = 'not so secret anymore'
+print(usingSecret._protectedInfo)
+
+print(usingSecret.getPrivate())
+usingSecret.setPrivate('not even you are safe')
+print(usingSecret.getPrivate())
+
